da6ae92afdbe97feae0e63dbe6835316a1570c16
mohamed <mohamed79135@gmail.com>
2020-12-07 17:22:16 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Login from './pages/Login'
+import SignUp from './pages/SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello wolrd</h1>
+      <Router>
+        <Switch>
+            <Route path="/SignUp">
+              <SignUp />
+            </Route>
+            <Route path="/Login">
+              <Login />
+            </Route>
+            <Route exact path="/">
+              <div>i am the pricipal page</div>
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
